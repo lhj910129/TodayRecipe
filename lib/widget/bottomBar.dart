a022991460c53da9e490f2eb6fe51b3a26b4721f
hyojinlee <lhj910129@gmail.com>
2022-03-11 17:54:52 +0900

    Accent Color Change

--- lib/widget/bottomBar.dart
+++ lib/widget/bottomBar.dart
@@ -10,7 +10,7 @@ class BottomBar extends StatelessWidget {
       child: Container(
         height: 90,
         child: TabBar(
-          labelColor: Colors.red[700],
+          labelColor: Colors.red[600],
           unselectedLabelColor: Colors.black26,
           indicatorColor: Colors.transparent,
           tabs: <Widget>[
